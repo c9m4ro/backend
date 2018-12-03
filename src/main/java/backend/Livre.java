40adb0f64188e4e22d6b4e6e2915d210eb1e2e16
AhlemFraj <44696951+AhlemFraj@users.noreply.github.com>
2018-12-03 15:20:49 +0100

    Update Livre.java

--- src/main/java/backend/Livre.java
+++ src/main/java/backend/Livre.java
@@ -1,5 +1,40 @@
 package backend;
 
 public class Livre {
+  private Integer nbPage;
+	private Integer Emprunts_total;
+	private String classification;
+	
+	
+	
+	
+	public Livre(Integer emprunts_total) {
+		super();
+		Emprunts_total = emprunts_total;
+	}
+	public Livre(Integer nbPage, Integer emprunts_total, String classification) {
+		super();
+		this.nbPage = nbPage;
+		Emprunts_total = emprunts_total;
+		this.classification = classification;
+	}
+	public Integer getNbPage() {
+		return nbPage;
+	}
+	public void setNbPage(Integer nbPage) {
+		this.nbPage = nbPage;
+	}
+	public Integer getEmprunts_total() {
+		return Emprunts_total;
+	}
+	public void setEmprunts_total(Integer emprunts_total) {
+		Emprunts_total = emprunts_total;
+	}
+	public String getClassification() {
+		return classification;
+	}
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
 
 }
